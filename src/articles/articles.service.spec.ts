d141c0e19876b3e75a2c3f8c7964a254d64759aa
Iqro-dev <igor.sokolowski.code@gmail.com>
2023-10-25 16:25:06 +0200

    fix(tests): articles and users service

--- src/articles/articles.service.spec.ts
+++ src/articles/articles.service.spec.ts
@@ -1,18 +1,86 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ArticlesService } from './articles.service';
+import { Model } from 'mongoose';
+import { Article } from './interfaces/article.interface';
+import { getModelToken } from '@nestjs/mongoose';
+import { createMock } from '@golevelup/ts-jest';
 
 describe('ArticlesService', () => {
-  let service: ArticlesService;
+  let articlesService: ArticlesService;
+  let articlesModel: Model<Article>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ArticlesService],
+      providers: [
+        ArticlesService,
+        {
+          provide: getModelToken('Article'),
+          useValue: {
+            find: jest.fn(),
+            findOne: jest.fn(),
+            create: jest.fn(),
+            findByIdAndUpdate: jest.fn(),
+            findByIdAndRemove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<ArticlesService>(ArticlesService);
+    articlesService = module.get<ArticlesService>(ArticlesService);
+    articlesModel = module.get<Model<Article>>(getModelToken('Article'));
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(articlesService).toBeDefined();
+  });
+
+  describe('getArticles', () => {
+    it('should return all articles', async () => {
+      const result = createMock<Article[]>();
+
+      jest.spyOn(articlesModel, 'find').mockResolvedValue(result);
+
+      expect(await articlesService.getArticles()).toBe(result);
+    });
+  });
+
+  describe('getArticle', () => {
+    it('should return a single article', async () => {
+      const result = createMock<Article>();
+
+      jest.spyOn(articlesModel, 'findOne').mockResolvedValue(result);
+
+      expect(await articlesService.getArticle('1')).toBe(result);
+    });
+  });
+
+  describe('createArticle', () => {
+    it('should create an article', async () => {
+      const result = createMock<Article>();
+
+      jest.spyOn(articlesModel, 'create').mockResolvedValue(result as any);
+
+      expect(await articlesService.createArticle(result)).toBe(result);
+    });
+  });
+
+  describe('updateArticle', () => {
+    it('should update an article', async () => {
+      const result = createMock<Article>();
+
+      jest.spyOn(articlesModel, 'findByIdAndUpdate').mockResolvedValue(result);
+
+      expect(await articlesService.updateArticle('1', result)).toBe(result);
+    });
+  });
+
+  describe('deleteArticle', () => {
+    it('should delete an article', async () => {
+      const result = createMock<Article>();
+
+      jest.spyOn(articlesModel, 'findByIdAndRemove').mockResolvedValue(result);
+
+      expect(await articlesService.deleteArticle('1')).toBe(result);
+    });
   });
 });
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,86 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { createMock } from '@golevelup/ts-jest';
+import { User } from './interfaces/user.interface';
+import { Model } from 'mongoose';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('UsersService', () => {
-  let service: UsersService;
+  let usersService: UsersService;
+  let usersModel: Model<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getModelToken('User'),
+          useValue: {
+            find: jest.fn(),
+            findOne: jest.fn(),
+            create: jest.fn(),
+            findByIdAndUpdate: jest.fn(),
+            findByIdAndRemove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<UsersService>(UsersService);
+    usersService = module.get<UsersService>(UsersService);
+    usersModel = module.get<Model<User>>(getModelToken('User'));
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(usersService).toBeDefined();
+  });
+
+  describe('getUsers', () => {
+    it('should return all users', async () => {
+      const result = createMock<User[]>();
+
+      jest.spyOn(usersModel, 'find').mockResolvedValue(result);
+
+      expect(await usersService.getUsers()).toBe(result);
+    });
+  });
+
+  describe('getUser', () => {
+    it('should return a single user', async () => {
+      const result = createMock<User>();
+
+      jest.spyOn(usersModel, 'findOne').mockResolvedValue(result);
+
+      expect(await usersService.getUser('1')).toBe(result);
+    });
+  });
+
+  describe('createUser', () => {
+    it('should create a user', async () => {
+      const result = createMock<User>();
+
+      jest.spyOn(usersModel, 'create').mockResolvedValue(result as any);
+
+      expect(await usersService.createUser(result)).toBe(result);
+    });
+  });
+
+  describe('updateUser', () => {
+    it('should update a user', async () => {
+      const result = createMock<User>();
+
+      jest.spyOn(usersModel, 'findByIdAndUpdate').mockResolvedValue(result);
+
+      expect(await usersService.updateUser('1', result)).toBe(result);
+    });
+  });
+
+  describe('deleteUser', () => {
+    it('should delete a user', async () => {
+      const result = createMock<User>();
+
+      jest.spyOn(usersModel, 'findByIdAndRemove').mockResolvedValue(result);
+
+      expect(await usersService.deleteUser('1')).toBe(result);
+    });
   });
 });
