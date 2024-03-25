aa9a10b64005089bb6d52ee7bf157cf53dcaadaf
Iqro-dev <igor.sokolowski.code@gmail.com>
2024-03-25 12:05:51 +0100

    feat(users): add validation to dto

--- src/articles/dtos/create-article.dto.ts
+++ src/articles/dtos/create-article.dto.ts
@@ -1,21 +1,33 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { IsDate, IsString, IsUUID, MinLength } from 'class-validator';
 
 export class CreateArticleDto {
   @ApiProperty()
+  @IsString()
+  @MinLength(5)
   readonly title: string;
 
   @ApiProperty()
+  @IsString()
+  @MinLength(10)
   readonly content: string;
 
   @ApiProperty()
+  @IsString()
+  @IsUUID()
   readonly authorID: string;
 
   @ApiProperty()
+  @IsString({ each: true })
   readonly likes: string[];
 
   @ApiProperty()
+  @IsString({ each: true })
   readonly dislikes: string[];
 
   @ApiProperty()
+  @IsDate()
+  @Transform(({ value }) => new Date(value + ''))
   readonly date: Date;
 }
